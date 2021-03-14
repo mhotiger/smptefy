ca8aa212b7de349eaa781638ea80717404186967
mhotiger <an@alexnoel.co>
2021-03-14 14:20:03 -0400

    update local functions environment config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,14 +4,15 @@ import cors from 'cors'
 import SpotifyWebApi  from 'spotify-web-api-node'
 import cookieParser from 'cookie-parser'
 import str from '@supercharge/strings'
+import bodyParser from 'body-parser';
 
 
 const app = express();
 
-const Spotify = new SpotifyWebApi({
-  clientId: functions.config().spotify.client_id,
-  clientSecret: functions.config().spotify.client_secret,
-})
+// const Spotify = new SpotifyWebApi({
+//   clientId: functions.config().spotify!.client_id,
+//   clientSecret: functions.config().spotify!.client_secret,
+// })
 
 const OAUTH_SCOPES = [
             'user-read-email',
@@ -28,6 +29,7 @@ const OAUTH_SCOPES = [
 
 app.use(cors());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded());
 
 app.get('/', (req, res)=>{
   res.send("HOME PAGE")
@@ -37,8 +39,16 @@ app.get('/redirect', (req,res)=>{
   const state:string = req.cookies.state || str.random(20);
   functions.logger.info("State verification: ", state);  
   res.cookie('state', state,{maxAge:3600000, secure: true, httpOnly:true});
-  const authorizeUrl = Spotify.createAuthorizeURL(OAUTH_SCOPES,state);
-  res.redirect(authorizeUrl);
+  // const authorizeUrl = Spotify.createAuthorizeURL(OAUTH_SCOPES,state);
+  // res.redirect(authorizeUrl);
+  res.send(`config: ${JSON.stringify(functions.config().spotify)}`)
+})
+
+app.get('/token', (req, res)=>{
+  const code = req.query.code;
+  const state = req.query.state;
+  res.send(`Code: ${code}\n State: ${state}`);
+
 })
 
 
