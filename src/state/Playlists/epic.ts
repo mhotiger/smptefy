ebea0313fcfd75d092c4cc8ae44af9b26e42e755
mhotiger <an@alexnoel.co>
2021-03-17 20:14:47 -0400

    Updated tracks to use default imageURL

--- src/state/Playlists/epic.ts
+++ src/state/Playlists/epic.ts
@@ -1,33 +1,36 @@
-import { Epic } from "redux-observable";
-import { filter, mergeMap, retryWhen, catchError, map} from 'rxjs/operators'
-import { AllActions, RootState } from "state";
-import { FETCH_PLAYLISTS } from "./types";
-import { isOfType} from 'typesafe-actions'
-import { getAuthToken } from "utils/auth";
-import { ajax } from "rxjs/ajax";
-import { addPlaylists } from "./action";
-import { requestErrorStrategy, retryStrategy } from "state/strategies";
-import { defer } from "rxjs";
+import { Epic } from 'redux-observable';
+import { filter, mergeMap, retryWhen, catchError, map } from 'rxjs/operators';
+import { AllActions, RootState } from 'state';
+import { FETCH_PLAYLISTS } from './types';
+import { isOfType } from 'typesafe-actions';
+import { getAuthToken } from 'utils/auth';
+import { ajax } from 'rxjs/ajax';
+import { addPlaylists } from './action';
+import { requestErrorStrategy, retryStrategy } from 'state/strategies';
+import { defer } from 'rxjs';
 
-export const playlistEpic: Epic<AllActions, AllActions, RootState, void>= (action$)=>{
-    return action$.pipe(
-        filter(isOfType(FETCH_PLAYLISTS)),
-        mergeMap(()=>{
-            
-            return defer(()=>ajax.getJSON('https://api.spotify.com/v1/me/playlists',{
-                'Authorization': `Bearer ${getAuthToken()}`,
-                'Content-Type': 'application/json'
-            })).pipe(
-                map((response: any)=>{
-                    return addPlaylists(response.items)
-                }),
-                retryWhen(retryStrategy()),
-                catchError(requestErrorStrategy())
-            )
-            
-        }),
-        catchError(requestErrorStrategy())
-    )
-}
-
-
+export const playlistEpic: Epic<AllActions, AllActions, RootState, void> = (
+	action$
+) => {
+	return action$.pipe(
+		filter(isOfType(FETCH_PLAYLISTS)),
+		mergeMap(() => {
+			return defer(() =>
+				ajax.getJSON(
+					'https://api.spotify.com/v1/me/playlists?limit=50',
+					{
+						Authorization: `Bearer ${getAuthToken()}`,
+						'Content-Type': 'application/json',
+					}
+				)
+			).pipe(
+				map((response: any) => {
+					return addPlaylists(response.items);
+				}),
+				retryWhen(retryStrategy()),
+				catchError(requestErrorStrategy())
+			);
+		}),
+		catchError(requestErrorStrategy())
+	);
+};
