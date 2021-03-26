ad95e86cf8530bd8f10aaaebfff8ee04daf85e6a
mhotiger <an@alexnoel.co>
2021-03-25 23:49:35 -0400

    base spotify load component

---
+++ src/components/common/SpotifyLoader.tsx
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+import { defer, Observable, Subscription } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { map, retryWhen } from 'rxjs/operators';
+import { retryStrategy } from 'state/strategies';
+import { getAuthToken } from 'utils/auth';
+
+interface State {
+	loading: boolean;
+	data?: any;
+}
+
+interface Props {
+	render: (state: State) => React.ReactNode;
+	path: string;
+}
+
+export class SpotifyLoader extends Component<Props, State> {
+	private sub?: Subscription;
+	public state: State = {
+		loading: false,
+	};
+
+	componentDidMount = () => {
+		this.setState({ loading: true });
+		this.sub = defer(() => {
+			ajax.getJSON(`https://api.spotify.com/v1/${this.props.path}`, {
+				Authorization: `Bearer ${getAuthToken()}`,
+				'Content-Type': 'application/json',
+			}).pipe(retryWhen(retryStrategy()));
+		}).subscribe((response) => {
+			this.setState({ data: response });
+		});
+	};
+
+	componentWillUnmount = () => {
+		if (this.sub) this.sub.unsubscribe();
+	};
+
+	render() {
+		return this.props.render(this.state);
+	}
+}
